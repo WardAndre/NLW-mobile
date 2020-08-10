585db11a500fd71e3be7390eb3defda68327eb27
Andre Ward <andre.ward62@gmail.com>
2020-08-10 02:46:01 -0300

    favorites done

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -6,22 +6,36 @@ import TeacherItem, { Teacher } from '../../components/TeacherItem'
 import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import { Feather } from '@expo/vector-icons'
 import api from '../../services/api'
+import AsyncStorage from '@react-native-community/async-storage'
 
 
 
 function TeacherList() {
     const [teachers, setTeachers] = useState([])
+    const [favorites, setFavorites] = useState<number[]>([])
     const [isFiltersVisible, setIsFiltersVisible] = useState(false)
 
     const [subject, setSubject] = useState('')
     const [week_day, setWeekDay] = useState('')
     const [time, setTime] = useState('')
 
+    function loadFavorites() {
+        AsyncStorage.getItem('favorites').then(response => {
+            if(response) {
+                const favoritedTeachers = JSON.parse(response)
+                const favoritedTeachersIds = favoritedTeachers.map((teacher: Teacher) => teacher.id)
+                setFavorites(favoritedTeachersIds)
+            }
+        }) 
+    }
+
     function handleToggleFiltersVisible() {
         setIsFiltersVisible(!isFiltersVisible)
     }
 
     async function handleFiltersSubmit() {
+        loadFavorites()
+
         const response = await api.get('classes', {
             params: {
                 subject,
@@ -93,7 +107,14 @@ function TeacherList() {
                     paddingBottom: 16
                 }}
             >
-                {teachers.map((teacher: Teacher) => <TeacherItem key={teacher.id} teacher={teacher}/>)}
+                {teachers.map((teacher: Teacher) => {
+                    return (
+                        <TeacherItem 
+                            key={teacher.id} 
+                            teacher={teacher}
+                            favorited={favorites.includes(teacher.id)}
+                        />)
+                    })}
             </ScrollView>
         </View>
     )
